ca432fd5f5270912f223e180533b89fd59fa42cc
luctpabc <91815207+luctpabc@users.noreply.github.com>
2021-12-01 16:49:45 +0700

    update

---
+++ quanlygiaydep/fDangNhap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyGiayDep
+{
+    public partial class fDangNhap : Form
+    {
+        public fDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if(txtMatKhau.Text != "123")
+            {
+                MessageBox.Show("Sai mật khẩu");
+                return;
+            }
+            fHoaDon f = new fHoaDon();
+            f.Show();
+            this.Hide();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fDangNhap_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = btnDangNhap;
+        }
+
+        private void txtMatKhau_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
